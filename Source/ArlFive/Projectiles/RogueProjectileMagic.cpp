c77ebd3564a201d0563294462ec702da627c5a04
오철민 <ocm10917@gmail.com>
2026-02-09 00:12:51 +0900

    Actor & Components (Projectile Attack)

---
+++ Source/ArlFive/Projectiles/RogueProjectileMagic.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.0f);
+	SphereComponent->SetCollisionProfileName("Projectile");
+	
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMoveComp"));
+	ProjectileMovementComponent->InitialSpeed = 2000.0f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+}
+
---
+++ Source/ArlFive/Projectiles/RogueProjectileMagic.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileMagic.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class ARLFIVE_API ARogueProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+
+public:
+	ARogueProjectileMagic();
+};
