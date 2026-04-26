189724766a51c8d39b88eb3caf31b96ab068143f
오철민 <ocm10917@gmail.com>
2026-04-27 01:54:13 +0900

    Interactable Item Chest

--- Source/ArlFive/World/RogueItemChest.cpp
+++ Source/ArlFive/World/RogueItemChest.cpp
@@ -4,23 +4,38 @@
 #include "RogueItemChest.h"
 
 
-// Sets default values
+
 ARogueItemChest::ARogueItemChest()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	
+	BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComp"));
+	RootComponent = BaseMeshComponent;
+	
+	LidMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LidMeshComp"));
+	LidMeshComponent->SetupAttachment(BaseMeshComponent);
 }
 
-// Called when the game starts or when spawned
+
 void ARogueItemChest::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	SetActorTickEnabled(true);
 }
 
-// Called every frame
+
 void ARogueItemChest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	
+	CurrenntAnimationPitch = FMath::FInterpConstantTo(CurrenntAnimationPitch, AnimationTargetPitch, DeltaTime, AnimationSpeed);
+	LidMeshComponent->SetRelativeRotation(FRotator(CurrenntAnimationPitch, 0.0f, 0.0f));
+	
+	if (FMath::IsNearlyEqual(CurrenntAnimationPitch, AnimationTargetPitch))
+	{
+		SetActorTickEnabled(false);
+	}
 }
 
